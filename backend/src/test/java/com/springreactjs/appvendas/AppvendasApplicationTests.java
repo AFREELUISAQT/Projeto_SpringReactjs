303388a6fea97caa7fd5536a2d9cfeeb6ad4bda3
Luisa Quinones Torres <afreeluisaqt@gmail.com>
2021-09-07 12:23:28 -0300

    Criando Projeto Spring Reactjs

---
+++ backend/src/test/java/com/springreactjs/appvendas/AppvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springreactjs.appvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
