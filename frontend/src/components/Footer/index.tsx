c54c88a61fc9e27a85121357b8c64281f52641fe
Luisa Quinones Torres <afreeluisaqt@gmail.com>
2021-09-07 14:28:38 -0300

    Incluindo Componentes Estaticos

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light">App desenvolvido por: <a href="https://www.linkedin.com/in/aludalotus/" target="_blank" rel="noreferrer">Luisa Quinones Torres</a></p>
+    <p className="text-light"><small><strong>Semana Spring ReactJS</strong><br/>
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+  </div>
+</footer>
+  );
+};
+
+export default Footer;
