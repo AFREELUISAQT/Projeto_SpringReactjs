0e0584c6ac640c06b31d8b926c0c852bb52e0b85
Luisa Quinones Torres <afreeluisaqt@gmail.com>
2021-09-11 19:53:04 -0300

    Busca por Grupo

--- backend/src/main/java/com/springreactjs/appvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/springreactjs/appvendas/repositories/SaleRepository.java
@@ -1,9 +1,19 @@
 package com.springreactjs.appvendas.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.springreactjs.appvendas.dto.SaleSuccessDTO;
+import com.springreactjs.appvendas.dto.SaleSumDTO;
 import com.springreactjs.appvendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-
+	
+	@Query("SELECT new com.springreactjs.appvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount))" + "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+	
+	@Query("SELECT new com.springreactjs.appvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals))" + "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
 }
--- backend/src/main/java/com/springreactjs/appvendas/service/SaleService.java
+++ backend/src/main/java/com/springreactjs/appvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.springreactjs.appvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.springreactjs.appvendas.dto.SaleDTO;
+import com.springreactjs.appvendas.dto.SaleSuccessDTO;
+import com.springreactjs.appvendas.dto.SaleSumDTO;
 import com.springreactjs.appvendas.entities.Sale;
 import com.springreactjs.appvendas.repositories.SaleRepository;
 import com.springreactjs.appvendas.repositories.SellerRepository;
@@ -25,6 +29,16 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 
 }
